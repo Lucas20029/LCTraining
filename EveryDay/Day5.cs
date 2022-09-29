399698bca75376940b133bc1aed5a0eb28fe3164
lucas20029 <lucas20029@163.com>
2022-09-29 23:14:14 +0800

    day5

--- EveryDay/Day5.cs
+++ EveryDay/Day5.cs
@@ -247,5 +247,105 @@ namespace DataStructure
             }
             return new string(text.ToArray());
         }
+
+        /// <summary>
+        /// #11. 盛最多水的容器
+        /// 双指针。从两头开始，向中间挤。 谁小移动谁（用未来可能的高度换宽度）
+        /// 例如：[1,8,6,2,5,4,8,3,7]
+        /// 一开始定位在 1,7， 那么容积是 min(1,7)*8 = 8
+        /// 此时，应该移动小的。
+        /// 因为如果移动7，那么宽度肯定是缩小了，而高度=min(1,X)肯定只会比1小，因此得不到最大容积。
+        /// 所以只能移动小的。
+        /// </summary>
+        /// <param name="height"></param>
+        /// <returns></returns>
+        public int MaxArea(int[] height)
+        {
+            int left = 0, right = height.Length - 1;
+            int max = 0;
+            while (left < right)
+            {
+                var volumn = Math.Min(height[left], height[right]) * (right - left);
+                max = Math.Max(max, volumn);
+                if (height[left] < height[right])
+                    left++;
+                else
+                    right--;
+            }
+            return max;
+        }
+
+
+        #region #31. 下一个排列
+        /// <summary>
+        /// 31. 下一个排列
+        ///例如，以下数列就是逐渐下一个排列：
+        ///12345
+        ///12345
+        ///12435
+        ///12453
+        ///12534
+        ///12543
+        ///13245
+        ///13254
+        ///13452
+        ///13524
+        ///13542
+        ///14235
+        ///14253....
+        ///....
+        ///15432
+        ///21345
+        ///21354
+        ///....
+        ///
+        ///1.从尾向前遍历，需要 逐渐递增。 一旦不递增，就是要修改该点
+        ///2.从末尾找第一个比该点大的数，拿过来（放到该点位置）
+        ///3.剩下的数字递增
+        /// </summary>
+        /// <param name="nums"></param>
+        public void NextPermutation(int[] nums)
+        {
+            
+            var pos = nums.Length - 1;
+            bool max = true;
+            for (int i = nums.Length - 2; i >= 0; i--)
+            {
+                if (nums[i] < nums[pos])
+                {
+                    pos = i; max = false; break;
+                }
+                pos--;
+            }
+            if (max)
+            {
+                PartialyReverse(nums, 0, nums.Length - 1);
+                return;
+            }
+            for (int i = nums.Length - 1; i >= 0; i--)
+            {
+                if (nums[i] > nums[pos])
+                {
+                    Swap(nums, i, pos);
+                    break;
+                }
+            }
+            PartialyReverse(nums, pos + 1, nums.Length - 1);
+        }
+        public void PartialyReverse(int[] nums, int start, int end)
+        {
+            while (start < end)
+            {
+                Swap(nums, start, end);
+                start++; end--;
+            }
+        }
+        public void Swap(int[] nums, int a, int b)
+        {
+            int t = nums[a];
+            nums[a] = nums[b];
+            nums[b] = t;
+        }
+        #endregion
     }
 }
